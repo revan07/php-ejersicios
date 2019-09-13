877e7a61ae87c0ae459b623d41a3a40f433bc07c
revan07 <imperiodecrystal@gmail.com>
2019-09-13 12:12:29 -0400

    Initial commit

---
+++ Untitled-1.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+<title>problema</title>
+    </head>
+    <body> 
+    <?php
+    $nombres[]="juan";
+    $nombres[]="pedro";
+    $nombres[]="ana";
+    for($f=0;$f<count($nombres);$f++)
+    {
+        echo $nombres[$f];
+        echo"<br>";
+    }
+    ?>  
+</body>
+</html>
---
+++ pagina2.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+      <title> problema </title>
+    </head>
+    <body> 
+    <?php
+    $diaSemana[]="Lunes";
+    $diaSemana[]="Martes";
+    $diaSemana[]="Miércoles";
+    $diaSemana[]="Jueves";
+    $diaSemana[]="Viernes";
+    $diaSemana[]="Sábado";
+    $diaSemana[]="Domingo";
+    echo "Primer elemento:".$diaSemana[0];
+    echo "<br>";
+    echo "Ultimo elemento:".$diaSemana[6];  
+    ?>
+
+  
+</body>
+</html>
